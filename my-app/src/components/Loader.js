aa18cf98341faef213e347d4de1ac40729ff7081
Sam Green <sam.green7777@yahoo.com>
2024-11-05 10:13:22 +0000

    add loader file to the project

---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "./css/Loader.module.css";
+
+function Loader() {
+    return (
+        <div classNamr={css.Loader}>
+
+        </div>
+    )
+}
+
+export default Loader

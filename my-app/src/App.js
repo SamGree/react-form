03af37f6c72a63d0b5b31391fe1c497a8dc45260
Sam Green <sam.green7777@yahoo.com>
2024-11-05 19:15:19 +0000

    add file name UncontrolledForm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,14 @@ import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
 import Content from "./components/Content";
 import ControlledForm from "./components/ControlledForm";
+import UncontrolledForm from "./components/UncontrolledForm";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
       <ControlledForm />
+      <UncontrolledForm />
       <NavBarForm />
       <Sidebar />
       <Content />

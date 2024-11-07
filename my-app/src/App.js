bb099fab8e33922814e5a681b49dc6bd4149a387
Sam Green <sam.green7777@yahoo.com>
2024-11-07 17:36:25 +0000

    add secrets js file to the project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,19 +2,19 @@ import css from "./App.module.css";
 // import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
 import NavBarForm from "./components/NavBarForm";
-import Content from "./components/Content";
-import ControlledForm from "./components/ControlledForm";
-import UncontrolledForm from "./components/UncontrolledForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+import ContentAPI from "./components/ContentAPI";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <ControlledForm />
-      <UncontrolledForm />
       <NavBarForm />
       <Sidebar />
-      <Content />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      <ContentAPI />
     </div>
   );
 }
